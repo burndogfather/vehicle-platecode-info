2683aa300947bba7cc4609a66801e088755c52ff
coco <squarehacker@kakao.com>
2023-12-06 19:18:39 +0900

    2023-12-06 19:18:39 업데이트!!!

--- main.go
+++ main.go
@@ -62,17 +62,14 @@ func requestHandler(res http.ResponseWriter, req *http.Request) {
 		
 		//사이트 캡쳐해서 버퍼생성
 		var outputStr string
-		if err := chromedp.Run(taskCtx, pdfGrabber("https://www.google.co.kr", "body", *outputStr)); err != nil {
-			//실패시 fail출력
-			res.Header().Set("Content-Type", "application/json")
-			resdata["status"] = "fail"
-			resdata["errormsg"] = err.Error()
-			output, err := json.Marshal(resdata)
-			if err != nil {
-				log.Fatalf("Error happened in JSON marshal. Err: %s", err)
-			}
-			res.Write(output)
-			return 
+		err := chromedp.Run(taskCtx,
+			emulation.SetUserAgentOverride("WebScraper 1.0"), //USER AGENT설정
+			chromedp.Navigate("https://www.google.co.kr"),
+			chromedp.WaitVisible("body", chromedp.ByQuery),
+			chromedp.InnerHTML(`body`, &outputStr),
+		)
+		if err != nil {
+			log.Fatalf("Error happened in JSON marshal. Err: %s", err)
 		}
 		
 		//성공시 PDF형태로출력
@@ -97,16 +94,5 @@ func requestHandler(res http.ResponseWriter, req *http.Request) {
 		}
 		res.Write(output)
 		return 
-	}
-}
-
-//PDF생성함수
-func pdfGrabber(url string, sel string, res string) chromedp.Tasks {
-	
-	return chromedp.Tasks{
-		emulation.SetUserAgentOverride("WebScraper 1.0"), //USER AGENT설정
-		chromedp.Navigate(url),
-		chromedp.WaitVisible(sel, chromedp.ByQuery),
-		chromedp.InnerHTML(`body`, &res),
 	}
 }

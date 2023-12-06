310ee18bf134a5947bd1323e5b246003217259cf
coco <squarehacker@kakao.com>
2023-12-06 19:42:40 +0900

    2023-12-06 19:42:40 업데이트!!!

--- main.go
+++ main.go
@@ -60,12 +60,19 @@ func requestHandler(res http.ResponseWriter, req *http.Request) {
 		defer cancel()
 		
 		//사이트 캡쳐해서 버퍼생성
-		var outputStr string
+		
 		err := chromedp.Run(taskCtx,
 			emulation.SetUserAgentOverride(`Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36`), //USER AGENT설정
 			chromedp.Navigate(`https://pkg.dadolcorp.com/test/multiselect_multi`),
 			chromedp.WaitVisible(`body`, chromedp.ByQuery),
-			chromedp.Value(`main.html_here > input#test3`, &outputStr),
+		)
+		if err != nil {
+			log.Fatalf("Error happened in ChromeDP. Err: %s", err)
+		}
+		
+		var outputStr string
+		err = chromedp.Run(taskCtx,
+			chromedp.Title(&outputStr),
 		)
 		if err != nil {
 			log.Fatalf("Error happened in ChromeDP. Err: %s", err)

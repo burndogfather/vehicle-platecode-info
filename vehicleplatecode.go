006db0c2565afe4b943ace983d8c0b6833fd7c68
coco <squarehacker@kakao.com>
2024-04-08 15:14:37 +0900

    2024-04-08 15:14:37 업데이트!!!

--- vehicleplatecode.go
+++ vehicleplatecode.go
@@ -100,11 +100,11 @@ func crawling(ctx context.Context, plateCode string, res http.ResponseWriter){
 	err := chromedp.Run(ctx,
 		emulation.SetUserAgentOverride(`Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36`), //USER AGENT설정
 		chromedp.Navigate(`https://www.car365.go.kr/web/contents/websold_vehicle.do`),
-		chromedp.WaitVisible(`body`),
+		chromedp.WaitVisible(`body`, chromedp.ByQuery),
 		chromedp.SendKeys(`input#search_str`, plateCode),
 		chromedp.EvaluateAsDevTools(`usedCarCompareInfo("search")`, nil),
-		chromedp.WaitVisible(`div.tblwrap_basic tbody#usedcarcompare_data`),
-		chromedp.Text(`div.tblwrap_basic tbody#usedcarcompare_data`, &carSearch),
+		chromedp.WaitVisible(`div.tblwrap_basic tbody#usedcarcompare_data`, chromedp.ByQuery),
+		chromedp.Text(`div.tblwrap_basic tbody#usedcarcompare_data`, &carSearch, chromedp.ByQuery),
 	)
 	if err != nil {
 		//실패시 fail출력
